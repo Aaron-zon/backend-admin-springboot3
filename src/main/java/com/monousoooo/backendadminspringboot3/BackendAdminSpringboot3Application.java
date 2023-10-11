31a315ba94a70749feadaa14301fcd48bffda834
zhoujiayao <zhou.jiayao@outlook.com>
2023-10-11 22:48:16 +0800

    springboot3 project initialization

---
+++ src/main/java/com/monousoooo/backendadminspringboot3/BackendAdminSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.monousoooo.backendadminspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAdminSpringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAdminSpringboot3Application.class, args);
+    }
+
+}
